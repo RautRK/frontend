50c326bb28db9ee016009e48a27c760dadaea40b
RautRK <149217891+RautRK@users.noreply.github.com>
2025-06-05 10:37:19 +0545

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import LoginSignupForm from '@/components/Signin'
 import React from 'react'
 
 const page = () => {
   return (
     <div className='flex justify-center items-center h-screen'>
-      <h1 className='text-3xl text-emerald-400'>Welcome to Home Page</h1>
+      <LoginSignupForm />
+
     </div>
   )
 }
